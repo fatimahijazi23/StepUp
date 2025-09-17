5bf96b2aa490f6bf0a3a2aaa576fea430e44f764
Fatima-Jawhari <fatimaibrahimaljawhari@gmail.com>
2025-09-17 23:25:19 +0300

    Company/JobSeekerDashboard(Rasha)

---
+++ aspteamWeb/Pages/Company/CompanyDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspteamWeb.Pages.Company
+{
+    public class ComanyDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ aspteamWeb/Pages/JobSeeker/JobSeekerDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspteamWeb.Pages.JobSeeker
+{
+    public class JobSeekerDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
