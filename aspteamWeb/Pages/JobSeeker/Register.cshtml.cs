907b8e50ebbb0ff1630640f62f3041ddec2e9b65
Rasha <termosrasha44@gmail.com>
2025-10-07 20:58:53 +0100

    Update Register.cshtml.cs

--- aspteamWeb/Pages/JobSeeker/Register.cshtml.cs
+++ aspteamWeb/Pages/JobSeeker/Register.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
-
+  
 namespace aspteamWeb.Pages.JobSeeker
 {
     public class RegisterModel : PageModel
