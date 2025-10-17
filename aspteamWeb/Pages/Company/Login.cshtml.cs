9f8ece8be2f2f9c29495cd66a3d72aea948b1522
Fatima-Jawhari <fatimaibrahimaljawhari@gmail.com>
2025-10-17 16:58:49 +0300

    Changenames

--- aspteamWeb/Pages/Company/Login.cshtml.cs
+++ aspteamWeb/Pages/Company/Login.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace teamProject.WEB.Pages
+namespace aspteamWeb.Pages.Company
 {
     public class LoginModel : PageModel
     { 
